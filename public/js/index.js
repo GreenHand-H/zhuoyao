01d250651ee849a891ef1b53bea52fd7f258e844
GreenHand-H <673196834@qq.com>
2019-06-24 21:18:45 +0800

    轮播图jq

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,39 @@ $(".part3 .video-tab .tab-item a").mouseover(function(){
     $(this).parent().addClass("on").siblings().removeClass("on");
     
 })
+$(".mt-right .tab-hd li a").mouseover(function(){
+    var dataVal=$(this).html();
+    $(`.grid-tab-bd .col-news[data-val=${dataVal}]`).addClass("dis").siblings(".col-news").removeClass("dis");
+    $(this).parent().addClass("on").siblings().removeClass("on");
+})
+var i=0;
+function moveTo(to){
+    if(to==undefined){
+        to=i+1;
+    }
+    i=to;
+    if(i==3){
+        i=0;
+    }
+    // console.log( i);
+    $(`.adPic a:eq(${i})`).css("left","0%").siblings().css("left","-100%");
+    $(`.adBtn a:eq(${i})`).addClass("on").siblings().removeClass("on");
+}
+var timer=setInterval(function(){
+    moveTo();
+},2000)
+$(".mt-center").hover(function(){
+    clearInterval(timer);
+},
+function(){
+    timer=setInterval(function(){
+        moveTo();
+    },2000)
+})
+$(".adBtn a").click(function(){
+    $(".adBtn a").each((i,elem)=>{ 
+        if(this==elem){
+           moveTo(i);
+        }
+    })
+})
