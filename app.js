385e330eaf35564efe719b90c437eff603477ef8
GreenHand-H <673196834@qq.com>
2019-06-30 13:53:28 +0800

    所有域名跨域

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const spriteRouter=require("./routers/sprite.js");
 var server=express();
 server.listen(3000);
 // server.use(cors());
-// server.all('*', function(req, res, next) {
-// 	res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-//     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header("X-Powered-By", ' 3.2.1')
-//     next();
-// });
+server.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 
 server.use(bodyParser.urlencoded({
 	extended:false
